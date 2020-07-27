326ad0155e8b13dceba7aad93417f422278dded5
James Monks <J.monks23@gmail.com>
2020-07-27 21:36:16 +0100

    extensions finished

--- src/app.js
+++ src/app.js
@@ -4,20 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      items: [
-        { name: "Buy shopping" },
-        { name: "Clean bathroom" },
-        { name: "Car's MOT" }
-      ],
-      newItem: ""
+      items: [],
+      newItem: "",
+      newItemPriority: ""
     },
     methods: {
       saveNewItem: function() {
         this.items.push({
-          name: this.newItem
+          name: this.newItem,
+          isPriority: this.newItemPriority
       });
         this.newItem="";
       },
+
+      highPriority: function() {
+          this.newItemPriority = true;
+      },
+
+      lowPriority: function() {
+          this.newItemPriority = false;
+      }
     }
   });
 });
