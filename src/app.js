607228879a9feb4c65511cf5d18df8079c711590
James Monks <J.monks23@gmail.com>
2020-07-27 16:25:18 +0100

    MVP finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
-import Vue from "vue";
+import Vue from 'vue';
 
-document.addEventListener("DOMContentLoaded", () => {
-    new Vue({
-        el: "#app",
-        })
-})
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      items: [
+        { name: "Buy shopping" },
+        { name: "Clean bathroom" },
+        { name: "Car's MOT" }
+      ],
+      newItem: ""
+    },
+    methods: {
+      saveNewItem: function() {
+        this.items.push({
+          name: this.newItem
+      });
+        this.newItem="";
+      },
+    }
+  });
+});
+
+
+
